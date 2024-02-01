93ff65e022390c506ddb6022ee0523fb5078d196
Oleksii Volkov <lifecoder.ua@gmail.com>
2024-02-01 21:06:59 +0200

    add basic structure

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Post from "./src/components/post/post";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Post />
+      <Post />
+      <Post />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/post/post.tsx
@@ -0,0 +1,7 @@
+import {Text} from "react-native";
+
+export default function Post() {
+  return (
+    <Text>I am a post stub</Text>
+  );
+}
