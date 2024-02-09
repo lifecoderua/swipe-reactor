ecd5adcc01fdf35bbb01d89e1d0b5174f54783d7
Oleksii Volkov <lifecoder.ua@gmail.com>
2024-02-09 22:54:47 +0200

    graphQL posts list - playground

--- src/components/playground/playground.tsx
+++ src/components/playground/playground.tsx
@@ -3,44 +3,89 @@ import {Text, View} from "react-native";
 import {gql, useQuery} from "@apollo/client";
 
 export default function Playground() {
+  // const GET_POST = gql`
+  //   query {
+  //     tag(name: "API") {
+  //       name,
+  //       nsfw,
+  //     },
+  //
+  //     node(id: "UG9zdDo1NTMxOTQ4") {
+  //       __typename
+  //       ... on Post {
+  //         text,
+  //         comments {
+  //       \t\ttext
+  //         },
+  //         attributes {
+  //             image {
+  //               comment
+  //               id
+  //             }
+  //           },
+  //           tags {
+  //             name
+  //           },
+  //           id
+  //       }
+  //     }
+  //   }
+  // `;
+
   const GET_POSTS = gql`
     query {
-      tag(name: "API") {
-        name,
-        nsfw,
-      },
-      
-      node(id: "UG9zdDo1NTMxOTQ4") {
-        __typename
-        ... on Post {
+    tag(name: null) {
+      name,
+      nsfw,
+      postPager(type: GOOD) {
+        posts(offset: 0) {
+          header,
           text,
-          comments {
-        \t\ttext
-          },
           attributes {
-              image {
-                comment
-                id
-              }
-            },
-            tags {
-              name
-            },
+            __typename
+            type
             id
+            insertId
+            image {
+              id
+              width
+              height
+              type
+              hasVideo
+              comment
+            }
+          },
+          tags {
+            name
+          },
+          id
         }
+        count
       }
-    }
-  `;
+    },
+  }`;
 
   const { loading, error, data } = useQuery(GET_POSTS);
 
   if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
 
+  const getImageSrc = (post: any) => {
+    const image = post.attributes[0].image;
+    const tags = post.tags.map((tag) => tag.name);
+    const imagePostId = atob(post.attributes[0].id).split(':')[1];
+    return `https://img10.joyreactor.cc/pics/post/${tags[0]}-${tags[1]}-${tags[2]}-${imagePostId}.${image.type}`;
+  }
+
   return (
     <View>
-      {data.node.attributes.map((attrs) => (
-        <Text key={attrs.image.id}>{attrs.image.id} *** {JSON.stringify(attrs)}</Text>
+      {data.tag.postPager.posts.map((post) => (
+        <Text key={post.id}>
+          <img
+            alt={post.tags[0].name}
+            src={getImageSrc(post)}
+          />
+        </Text>
       ))}
     </View>
   );
