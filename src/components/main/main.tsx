fc92533261ae4b45e7054c2cb389717241edb75a
Oleksii Volkov <lifecoder.ua@gmail.com>
2024-02-10 12:10:40 +0200

    graphQL posts list - render with broken nav

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,84 +1,56 @@
-import React, { useState, useLayoutEffect, useRef } from 'react';
-import { FlatList, Dimensions, StyleSheet } from 'react-native';
-import Post from '../post/post';
-
-const data = [
-  {
-    id: '1',
-    title: 'First Post',
-    content: 'This is my first post',
-    imageURL: 'https://picsum.photos/200/300',
-  },
-  {
-    id: '2',
-    title: 'Second Post',
-    content: 'This is my second post',
-    imageURL: 'https://picsum.photos/300/600',
-  },
-  {
-    id: '3',
-    title: 'Third Post',
-    content: 'This is my third post',
-    imageURL: 'https://picsum.photos/600/300',
-  },
-];
+import React from 'react';
+import {gql, useQuery} from "@apollo/client";
+import PostList from "../postList/postList";
 
 export default function Main() {
-  const [index, setIndex] = useState(0);
-  const flatListRef = useRef<FlatList<any>>(null);
-  const ITEM_HEIGHT = Dimensions.get('window').height * 0.8;
 
-  // set navigation
-  useLayoutEffect(() => {
-    const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.key === 'w') {
-        setIndex((prevIndex) => Math.max(0, prevIndex - 1));
-      } else if (e.key === 's') {
-        setIndex((prevIndex) => Math.min(data.length - 1, prevIndex + 1));
+  const GET_POSTS = gql`
+    query GetPosts($tagName: String, $type: PostLineType!, $page: Int, $offset: Int) {
+    tag(name: $tagName) {
+      name,
+      nsfw,
+      postPager(type: $type) {
+        posts(page: $page, offset: $offset) {
+          header,
+          text,
+          nsfw,
+          attributes {
+            __typename
+            type
+            id
+            insertId
+            image {
+              id
+              width
+              height
+              type
+              hasVideo
+              comment
+            }
+          },
+          tags {
+            name
+          },
+          id
+        }
+        count
       }
-    };
+    },
+  }`;
 
-    window.addEventListener('keydown', handleKeyDown);
+  const { loading, error, data } = useQuery(GET_POSTS, {
+    variables: {
+      tagName: null,
+      type: 'GOOD',
+      page: 0,
+      offset: 0,
+    },
+  });
 
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []);
-
-  useLayoutEffect(() => {
-    if (flatListRef.current) {
-      flatListRef.current.scrollToIndex({ index, animated: true });
-    }
-  }, [index]);
-
-  const renderItem = ({ item }) => (
-    <Post title={item.title} imageURL={item.imageURL} />
-  );
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
 
   return (
-    <FlatList
-      ref={flatListRef}
-      data={data}
-      renderItem={renderItem}
-      keyExtractor={(item) => item.id}
-      pagingEnabled
-      horizontal={false}
-      snapToInterval={Dimensions.get('window').height * 0.8}
-      showsVerticalScrollIndicator={false}
-      style={styles.container}
-      contentContainerStyle={styles.contentContainer}
-      getItemLayout={(data, index) => (
-        {length: ITEM_HEIGHT, offset: ITEM_HEIGHT * index, index}
-      )}
-    />
-  );
+    <PostList posts={data.tag.postPager.posts} />
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  contentContainer: {
-    height: Dimensions.get('window').height * data.length * 0.8,
-  },
-});
---
+++ src/components/postList/postList.tsx
@@ -0,0 +1,108 @@
+import {Dimensions, StyleSheet, FlatList, Text} from "react-native";
+import React, {useLayoutEffect, useRef, useState} from "react";
+import Post from "../post/post";
+
+type PostAttributes = {
+  __typename: string,
+  type: string,
+  id: string,
+  insertId: string,
+  image: {
+    id: string,
+    width: number,
+    height: number,
+    type: string,
+    hasVideo: boolean,
+    comment: string,
+  },
+}
+
+type Post = {
+  id: string,
+  text: string,
+  attributes: PostAttributes[],
+}
+
+type PostListProps = {
+  posts: Post[],
+}
+
+export default function PostList(props: PostListProps) {
+  const [index, setIndex] = useState(0);
+  const flatListRef = useRef<FlatList<any>>(null);
+  const ITEM_HEIGHT = Dimensions.get('window').height * 0.8;
+
+  // set navigation
+  useLayoutEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'w') {
+        setIndex((prevIndex) => Math.max(0, prevIndex - 1));
+      } else if (e.key === 's') {
+        setIndex((prevIndex) => Math.min(props.posts.length - 1, prevIndex + 1));
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  useLayoutEffect(() => {
+    if (flatListRef.current) {
+      flatListRef.current.scrollToIndex({ index, animated: true });
+    }
+  }, [index]);
+
+  const getImageSrc = (post: any) => {
+    if (!post.attributes?.[0]?.image) {
+      return false;
+    }
+
+    const image = post.attributes[0].image;
+    const tags = post.tags.map((tag) => tag.name);
+    const imagePostId = atob(post.attributes[0].id).split(':')[1];
+    return `https://img10.joyreactor.cc/pics/post/${tags[0]}-${tags[1]}-${tags[2]}-${imagePostId}.${image.type}`;
+  }
+
+  const renderItem = (post) => getImageSrc(post) ? renderImage(post) : renderPlaceholder(post);
+
+  const renderImage = (post) => (
+    <Post title={post.id} imageURL={getImageSrc(post) as string} />
+  );
+
+  const renderPlaceholder = (post) => (
+    <Text>nothing cool here</Text>
+  );
+
+  return (
+    <FlatList
+      ref={flatListRef}
+      data={props.posts}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+      pagingEnabled
+      horizontal={false}
+      snapToInterval={Dimensions.get('window').height * 0.8}
+      showsVerticalScrollIndicator={false}
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      getItemLayout={(data, index) => (
+        {length: ITEM_HEIGHT, offset: ITEM_HEIGHT * index, index}
+      )}
+    />
+  );
+}
+
+// TODO use global constant? And always provide a 10 items buffer
+const pagerLength = 10;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainer: {
+    height: Dimensions.get('window').height * pagerLength * 0.8,
+  },
+});
